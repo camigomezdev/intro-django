206fc092fc5335b352b4eb1f8013c17f644c01ec
Cami Gomez <camigomez@hugeinc.com>
2022-12-15 21:41:18 -0500

    Config mysql db

--- ciudadesDelMundo/settings.py
+++ ciudadesDelMundo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ciudadesDelMundo.urls'
+ROOT_URLCONF = 'cityDelMundo.urls'
 
 TEMPLATES = [
     {
@@ -76,10 +77,18 @@ WSGI_APPLICATION = 'ciudadesDelMundo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+pymysql.version_info = (1, 4, 2, 'final', 0)
+
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'travels',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
